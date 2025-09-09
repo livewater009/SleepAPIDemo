653b19e78aab0a8ca571631c5a0c839aba915b8d
livewizard93 <147813832+livewater009@users.noreply.github.com>
2025-09-08 19:07:16 -0700

    Updated

--- app/src/main/java/com/androidphotoapp/sleepapidemo/SleepRequestManager.kt
+++ app/src/main/java/com/androidphotoapp/sleepapidemo/SleepRequestManager.kt
@@ -2,6 +2,8 @@ package com.androidphotoapp.sleepapidemo
 
 import android.Manifest
 import android.content.Context
+import android.hardware.Sensor
+import android.hardware.SensorManager
 import android.util.Log
 import android.widget.Toast
 import androidx.annotation.RequiresPermission
@@ -10,9 +12,23 @@ import com.google.android.gms.location.ActivityRecognition
 import com.google.android.gms.location.SleepSegmentRequest
 
 class SleepRequestManager(private val context: Context) {
+    private val sensorManager = context.getSystemService(Context.SENSOR_SERVICE) as SensorManager
+
+    fun isSensorAvailable(sensorType: Int): Boolean {
+        val sensor = sensorManager.getDefaultSensor(sensorType)
+        return sensor != null
+    }
 
     @RequiresPermission(Manifest.permission.ACTIVITY_RECOGNITION)
     fun subscribeToSleepUpdates() {
+        val accelerometerAvailable = isSensorAvailable(Sensor.TYPE_ACCELEROMETER)
+        val gyroscopeAvailable = isSensorAvailable(Sensor.TYPE_GYROSCOPE)
+        val lightSensorAvailable = isSensorAvailable(Sensor.TYPE_LIGHT)
+
+        Log.d("SensorCheck", "Accelerometer: $accelerometerAvailable")
+        Log.d("SensorCheck", "Gyroscope: $gyroscopeAvailable")
+        Log.d("SensorCheck", "Light: $lightSensorAvailable")
+
         // TODO: Request Sleep API updates
         val task = ActivityRecognition.getClient(context)
             .requestSleepSegmentUpdates(
